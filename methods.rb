67cb1ce5006c8df0b45321b9ae637584074fad49
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-18 05:55:05 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet person_name
+  puts "Hello, #{person_name}!"
+end 
+
+def greet_with_default name = "programmer"
+  puts "Hello, #{name}!"
+end
+
+def add num1, num2
+  num1 + num2
+end
+
+def halve num 
+  num.class == Integer ? num/2 : nil 
+end
